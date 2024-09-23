a658f1133ef4ff8f44f3a002cf12ded6b97cabd6
Suyeon Kim <57708995+syk001108@users.noreply.github.com>
2024-09-23 15:47:15 +0900

    FEAT: Change menu icons and create submenus for Resources

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   CDBSidebar,
   CDBSidebarContent,
   CDBSidebarFooter,
   CDBSidebarHeader,
   CDBSidebarMenu,
-  CDBSidebarMenuItem
+  CDBSidebarMenuItem,
 } from "cdbreact";
 import { NavLink } from "react-router-dom";
 
 const Sidebar = () => {
+  const [isResourcesOpen, setResourcesOpen] = useState(false);
+
+  const toggleResourcesMenu = () => {
+    setResourcesOpen(!isResourcesOpen);
+  };
+
   return (
     <div
       style={{ display: "flex", height: "100%", overflow: "scroll initial" }}
@@ -19,48 +25,82 @@ const Sidebar = () => {
           <a
             href="/"
             className="text-decoration-none"
-            style={{ color: "inherit" }}
+            style={{ color: "inherit", fontSize: "30px" }}
           >
-            Kanary
+            kanary
           </a>
         </CDBSidebarHeader>
 
         <CDBSidebarContent className="sidebar-content">
           <CDBSidebarMenu>
             <NavLink exact to="/" activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="columns">Overview</CDBSidebarMenuItem>
+              <CDBSidebarMenuItem icon="tachometer-alt">Overview</CDBSidebarMenuItem>
             </NavLink>
             <NavLink exact to="/topology" activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="table">Topology</CDBSidebarMenuItem>
+              <CDBSidebarMenuItem icon="share-alt">Topology</CDBSidebarMenuItem>
             </NavLink>
             <NavLink exact to="/node" activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="user">Node</CDBSidebarMenuItem>
+              <CDBSidebarMenuItem icon="server">Node</CDBSidebarMenuItem>
             </NavLink>
-            <NavLink exact to="/resources" activeClassName="activeClicked">
-              <CDBSidebarMenuItem icon="chart-line">
+
+            {/* Resources with a toggle for the submenus */}
+            <div>
+              <CDBSidebarMenuItem
+                icon="database"
+                onClick={toggleResourcesMenu}
+                style={{ cursor: "pointer" }}
+              >
                 Resources
               </CDBSidebarMenuItem>
-            </NavLink>
+              {isResourcesOpen && (
+                <div style={{ paddingLeft: "20px" }}>
+                  <NavLink exact to="/resources/pods" activeClassName="activeClicked">
+                    <CDBSidebarMenuItem icon="cube">Pods</CDBSidebarMenuItem>
+                  </NavLink>
+                  <NavLink exact to="/resources/volume" activeClassName="activeClicked">
+                    <CDBSidebarMenuItem icon="hdd">Volume</CDBSidebarMenuItem>
+                  </NavLink>
+                  <NavLink exact to="/resources/service" activeClassName="activeClicked">
+                    <CDBSidebarMenuItem icon="network-wired">Service</CDBSidebarMenuItem>
+                  </NavLink>
+                  <NavLink exact to="/resources/daemonset" activeClassName="activeClicked">
+                    <CDBSidebarMenuItem icon="tasks">Daemonset</CDBSidebarMenuItem>
+                  </NavLink>
+                </div>
+              )}
+            </div>
+
             <NavLink exact to="/event" activeClassName="activeClicked">
               <CDBSidebarMenuItem icon="exclamation-circle">Event</CDBSidebarMenuItem>
             </NavLink>
           </CDBSidebarMenu>
         </CDBSidebarContent>
 
-        
         <CDBSidebarFooter className="sidebar-footer">
           <CDBSidebarContent>
-            <NavLink exact to="/settings" activeClassName="activeClicked" style={{ textDecoration: "none", color: "inherit" }}>
-              <CDBSidebarMenuItem icon="columns">Settings</CDBSidebarMenuItem>
+            <NavLink
+              exact
+              to="/settings"
+              activeClassName="activeClicked"
+              style={{ textDecoration: "none", color: "inherit" }}
+            >
+              <CDBSidebarMenuItem icon="cog">Settings</CDBSidebarMenuItem>
             </NavLink>
-            <a href="https://github.com/orgs/KanaryOrgs/repositories" 
-               target="_blank" 
-               rel="noopener noreferrer" 
-               style={{ textDecoration: "none", color: "inherit" }}>
-               <CDBSidebarMenuItem icon="table">Help</CDBSidebarMenuItem>
+            <a
+              href="https://github.com/orgs/KanaryOrgs/repositories"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ textDecoration: "none", color: "inherit" }}
+            >
+              <CDBSidebarMenuItem icon="question-circle">Help</CDBSidebarMenuItem>
             </a>
-            <NavLink exact to="/logout" activeClassName="activeClicked" style={{ textDecoration: "none", color: "inherit" }}>
-              <CDBSidebarMenuItem icon="user">Log Out</CDBSidebarMenuItem>
+            <NavLink
+              exact
+              to="/logout"
+              activeClassName="activeClicked"
+              style={{ textDecoration: "none", color: "inherit" }}
+            >
+              <CDBSidebarMenuItem icon="sign-out-alt">Log Out</CDBSidebarMenuItem>
             </NavLink>
           </CDBSidebarContent>
         </CDBSidebarFooter>
