3d8936e08ed230fc8df74507e613ac0bfb9710a3
kpryu6 <kyongpyo0626@gmail.com>
2024-10-07 16:04:31 +0900

    [FEAT] connect to backend

--- src/pages/Overview/Overview.js
+++ src/pages/Overview/Overview.js
@@ -1,90 +1,43 @@
 import React, { useState, useEffect } from "react";
-import { Line } from 'react-chartjs-2';
+import { Line } from "react-chartjs-2";
 import Sidebar from "../../Sidebar";
 import Navbar from "../../Navbar";
-import { cpuData, ramData } from "./overview-dummy";
+import { useQuery } from "react-query";
 import "./Overview.css";
+import { fetchData, countNodeStatus, countPodStatus, confirm } from "../Utils";
+import { chartOptions } from "../Chart";
 
-// Chart.js components 등록
-import { CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js';
-import { Chart as ChartJS } from 'chart.js/auto';
-ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
-
-const chartOptions = {
-  maintainAspectRatio: false,
-  scales: {
-    x: {
-      beginAtZero: true,
-    },
-    y: {
-      beginAtZero: true,
-    },
-  },
-};
-
-// Backend에서 데이터 가져오기(fetch)
-const fetchData = async (url, setData, setLoading, setError) => {
-  try {
-    setLoading(true);
-    const response = await fetch(url);
-    const result = await response.json();
-    setData(result);
-  } catch (error) {
-    setError(true);
-  } finally {
-    setLoading(false);
-  }
-};
-
-// // 노드 상태 JSON에서 뽑기
-// const countNodeStatus = (nodes) => {
-//   let noConnection = 0;
-//   let notReady = 0;
-//   let ready = 0;
-
-//   nodes.forEach(node => {
-//     if (node.status === "Ready") {
-//       ready++;
-//     } else if (node.status === "NotReady") {
-//       notReady++;
-//     } else {
-//       noConnection++;
-//     }
-//   });
-
-//   return [`No Connection: ${noConnection}`, `Not Ready: ${notReady}`, `Ready: ${ready}`];
-// };
-
+// Swagger UI : /swagger/index.html
 export const Overview = () => {
+  // 해당 State 들
+  const [cpuData, setCpuData] = useState(null);
+  const [ramData, setRamData] = useState(null);
 
-  // // 해당 State 들
-  // const [cpuData, setCpuData] = useState(null);
-  // const [ramData, setRamData] = useState(null);
-  // const [nodes, setNodes] = useState([]);
-  // const [events, setEvents] = useState(null);
-  // const [loading, setLoading] = useState(true);
-  // const [error, setError] = useState(false);
+  // 컴포넌트 마운트 시 데이터 fetch
+  // useQuery로 API 호출 및 상태 관리
+  const {
+    data: nodes,
+    isLoading: loadingNodes,
+    error: errorNodes,
+  } = useQuery("nodes", () => fetchData("http://localhost:8080/v1/nodes"));
 
-  // // 컴포넌트 마운트 시 데이터 fetch
-  // useEffect(() => {
-  //   fetchData("/api/cpu-usage", setCpuData, setLoading, setError);
-  //   fetchData("/api/ram-usage", setRamData, setLoading, setError);
-  //   fetchData("http://localhost:8080/v1/nodes", setNodes, setLoading, setError);
-  // }, []);
+  const {
+    data: pods,
+    isLoading: loadingPods,
+    error: errorPods,
+  } = useQuery("pods", () => fetchData("http://localhost:8080/v1/pods"));
 
-  // if (loading) {
-  //   return <p>Loading...</p>;
-  // }
+  confirm(loadingNodes, errorNodes);
+  confirm(loadingPods, errorPods);
 
-  // if (error) {
-  //   return <p>Error loading data</p>;
-  // }
-
-  // const nodeStatus = countNodeStatus(nodes);
+  const nodeStatus = countNodeStatus(nodes);
+  const podStatus = countPodStatus(pods);
 
   return (
     <div className="d-flex E">
-      <div><Sidebar /></div>
+      <div>
+        <Sidebar />
+      </div>
       <div className="main-content">
         <Navbar />
         <div className="event-content">
@@ -97,9 +50,8 @@ export const Overview = () => {
         <div className="content-section">
           <div className="card-container">
             {/* backend와 통신할 때 이런식으로 */}
-            {/* <StatusCard title="Nodes" statuses={nodeStatuses} /> */}
-            <StatusCard title="Nodes" statuses={["No Connection: 0", "Not Ready: 0", "Ready: 1"]} />
-            <StatusCard title="Pods" statuses={["Error: 1", "Pending: 0", "Running: 15"]} />
+            <StatusCard title="Nodes" statuses={nodeStatus} />
+            <StatusCard title="Pods" statuses={podStatus} />
           </div>
 
           <div className="chart-container">
@@ -109,12 +61,31 @@ export const Overview = () => {
 
           <div className="info-event-container">
             <div className="info-cards">
-              <TopCard title="Top CPU Intensive Nodes" statuses={["Master Node"]} />
-              <TopCard title="Top RAM Intensive Nodes" statuses={["Master Node"]} />
-              <TopCard title="Top CPU Intensive Pods" statuses={["etcd-master"]} />
-              <TopCard title="Top RAM Intensive Pods" statuses={["kube-apiserver-master"]} />
+              <TopCard
+                title="Top CPU Intensive Nodes"
+                statuses={["Master Node"]}
+              />
+              <TopCard
+                title="Top RAM Intensive Nodes"
+                statuses={["Master Node"]}
+              />
+              <TopCard
+                title="Top CPU Intensive Pods"
+                statuses={["etcd-master"]}
+              />
+              <TopCard
+                title="Top RAM Intensive Pods"
+                statuses={["kube-apiserver-master"]}
+              />
             </div>
-            <EventCard events={[{ message: "[Warning] Pod 'Nginx-pod' restarted at xx:xx : Out of Memory" }]} />
+            <EventCard
+              events={[
+                {
+                  message:
+                    "[Warning] Pod 'Nginx-pod' restarted at xx:xx : Out of Memory",
+                },
+              ]}
+            />
           </div>
         </div>
       </div>
@@ -135,8 +106,12 @@ const StatusCard = ({ title, statuses }) => (
 const ChartCard = ({ title, data }) => (
   <div className="chart-card">
     <h4>{title}</h4>
-    <div style={{ height: '200px' }}>
-      {data ? <Line data={data} options={chartOptions} /> : <p>Loading chart...</p>}
+    <div style={{ height: "200px" }}>
+      {data ? (
+        <Line data={data} options={chartOptions} />
+      ) : (
+        <p>Loading chart...</p>
+      )}
     </div>
   </div>
 );
@@ -156,7 +131,9 @@ const EventCard = ({ events }) => (
     <h4>Events</h4>
     {events ? (
       events.map((event, index) => (
-        <p key={index} className="event-warning">{event.message}</p>
+        <p key={index} className="event-warning">
+          {event.message}
+        </p>
       ))
     ) : (
       <p>Loading events...</p>
--- src/pages/Topology/Topology.js
+++ src/pages/Topology/Topology.js
@@ -1,77 +1,72 @@
-import React, { useEffect, useState, useRef } from 'react';
-import { ForceGraph2D } from 'react-force-graph';
-import { podData } from './topoloy-dummy'
-import Sidebar from '../../Sidebar';
-import Navbar from '../../Navbar';
-import { CDBBtn, CDBContainer, CDBBtnGrp } from 'cdbreact';
-import "./Topology.css"
-import Dropdown from '../Dropdown';
+import React, { useEffect, useState, useRef } from "react";
+import { ForceGraph2D } from "react-force-graph";
+import Sidebar from "../../Sidebar";
+import Navbar from "../../Navbar";
+import { CDBBtn, CDBContainer, CDBBtnGrp } from "cdbreact";
+import "./Topology.css";
+import Dropdown from "./Dropdown/Dropdown";
+import { useQuery } from "react-query";
+import { fetchData, confirm } from "../Utils";
 
 export const Topology = () => {
   const [nodes, setNodes] = useState([]);
-  // const [links, setLinks] = useState([]);
   const [namespaces, setNamespaces] = useState([]);
-  const [selectedNamespace, setSelectedNamespace] = useState('default');
-  const [selectedMetric, setSelectedMetric] = useState('cpu_capacity');  // This can be 'cpu_capacity' or 'ram_capacity'
+  const [selectedNamespace, setSelectedNamespace] = useState("default");
+  const [selectedMetric, setSelectedMetric] = useState("cpu_capacity"); // This can be 'cpu_capacity' or 'ram_capacity'
   const fgRef = useRef(); // Ref for accessing the force graph instance
 
+  const {
+    data: pods,
+    isLoading: loadingPods,
+    error: errorPods,
+  } = useQuery("pods", () => fetchData("http://localhost:8080/v1/pods"));
+  confirm(loadingPods, errorPods);
+
   useEffect(() => {
+    const graphNodes = pods.map((pod, index) => {
+      const angle = (2 * Math.PI * index) / pods.length;
+      const radius = 70; // Adjust radius to spread out the nodes
+      return {
+        id: pod.name,
+        name: pod.name,
+        ip: pod.ip,
+        status: pod.status,
+        namespace: pod.namespace,
+        images: pod.images.join(", "),
+        labels: JSON.stringify(pod.labels),
+        cpu_capacity: pod.cpu_capacity,
+        ram_capacity: pod.ram_capacity,
+        restarts: pod.restarts,
+        x: -50 + radius * Math.cos(angle), // 400 is center offset
+        y: -50 + radius * Math.sin(angle), // 400 is center offset
+        fx: -50 + radius * Math.cos(angle),
+        fy: -50 + radius * Math.sin(angle),
+      };
+    });
 
-      const graphNodes = podData.map((pod,index) => {
-        const angle = 2 * Math.PI * index / podData.length;
-        const radius = 70; // Adjust radius to spread out the nodes
-        return {
-          id: pod.name,
-          name: pod.name,
-          ip : pod.ip,
-          status: pod.status,
-          namespace: pod.namespace,
-          images: pod.images.join(", "),
-          labels: JSON.stringify(pod.labels),
-          cpu_capacity: pod.cpu_capacity,
-          ram_capacity: pod.ram_capacity,
-          restarts: pod.restarts,
-          x: -50 + radius * Math.cos(angle), // 400 is center offset
-          y: -50 + radius * Math.sin(angle), // 400 is center offset
-          fx: -50 + radius * Math.cos(angle),
-          fy: -50 + radius * Math.sin(angle)
-        };
-      });
+    setNodes(graphNodes);
 
-      // const graphLinks = [];
-      // graphNodes.forEach((node, idx) => {
-      //   graphNodes.forEach((otherNode, otherIdx) => {
-      //     if (idx !== otherIdx && node.namespace === otherNode.namespace) {
-      //       graphLinks.push({ source: node.id, target: otherNode.id });
-      //     }
-      //   });
-      // });
-  
-      setNodes(graphNodes);
-      
-      setNamespaces([...new Set(podData.map(pod => pod.namespace))]);
- 
-      // setLinks(graphLinks);
+    setNamespaces([...new Set(pods.map((pod) => pod.namespace))]);
 
+    // setLinks(graphLinks);
+  }, []);
 
-      
-    }, []);
+  const handleNamespaceChange = (namespace) => {
+    setSelectedNamespace(namespace);
+  };
 
-    const handleNamespaceChange = (namespace) => {
-      setSelectedNamespace(namespace);
-    };
+  const filteredNodes = nodes.filter(
+    (node) => node.namespace === selectedNamespace
+  );
 
-
-    const filteredNodes = nodes.filter(node => node.namespace === selectedNamespace);
-
-    const handleNodeClick = node => {
-        const tooltip = document.getElementById('tooltip');
-        if (node) {
-            const { x, y } = fgRef.current.graph2ScreenCoords(node.x, node.y);
-            tooltip.style.display = 'block';
-            tooltip.style.left = `${x + 20}px`;
-            tooltip.style.top = `${y}px`;
-            tooltip.innerHTML = `
+  const handleNodeClick = (node) => {
+    const tooltip = document.getElementById("tooltip");
+    if (node) {
+      const { x, y } = fgRef.current.graph2ScreenCoords(node.x, node.y);
+      tooltip.style.display = "block";
+      tooltip.style.left = `${x + 20}px`;
+      tooltip.style.top = `${y}px`;
+      tooltip.innerHTML = `
                 <div class='close' onclick='this.parentElement.style.display="none";'>&times;</div>
                 <h4>${node.name}</h4>
                 <strong>Namespace:</strong> ${node.namespace}<br/>
@@ -82,53 +77,58 @@ export const Topology = () => {
                 <strong>Container Images:</strong> <pre>${node.images}</pre>
                 <strong>Restarts:</strong> <pre>${node.restarts}</pre>
             `;
-        }
-    };
-    
-  
-    return (
-      <div className="d-flex E">
-        <div><Sidebar/></div>
-        <div className="main-content">
-          <Navbar/>
-          <div className="event-content">
-            <div className="event-header">
-                <h2>Topology</h2>
-                <p>Kubernetes Cluster Topology</p>
-            </div>
+    }
+  };
+
+  return (
+    <div className="d-flex E">
+      <div>
+        <Sidebar />
+      </div>
+      <div className="main-content">
+        <Navbar />
+        <div className="event-content">
+          <div className="event-header">
+            <h2>Topology</h2>
+            <p>Kubernetes Cluster Topology</p>
           </div>
+        </div>
 
-          <div id="tooltip"></div>
+        <div id="tooltip"></div>
 
-          <div className='settings'>
-            <CDBContainer>
+        <div className="settings">
+          <CDBContainer>
             <CDBBtnGrp>
-              <CDBBtn color='primary' size='large'
-                className={selectedMetric === 'cpu_capacity' ? 'active' : ''}
-                onClick={() => setSelectedMetric('cpu_capacity')}
-                style={{ marginRight: '10px' }}
+              <CDBBtn
+                color="primary"
+                size="large"
+                className={selectedMetric === "cpu_capacity" ? "active" : ""}
+                onClick={() => setSelectedMetric("cpu_capacity")}
+                style={{ marginRight: "10px" }}
               >
                 CPU
               </CDBBtn>
-              <CDBBtn color='primary' size='large'
-                className={selectedMetric === 'ram_capacity' ? 'active' : ''}
-                onClick={() => setSelectedMetric('ram_capacity')}
-                style={{ marginRight: '10px' }}
+              <CDBBtn
+                color="primary"
+                size="large"
+                className={selectedMetric === "ram_capacity" ? "active" : ""}
+                onClick={() => setSelectedMetric("ram_capacity")}
+                style={{ marginRight: "10px" }}
               >
                 Memory
               </CDBBtn>
             </CDBBtnGrp>
           </CDBContainer>
 
-          <div className='dropdown'>
+          <div className="dropdown">
             <Dropdown
-              label={`Namespace: ${selectedNamespace || 'Select'}`}
+              label={`Namespace: ${selectedNamespace || "Select"}`}
               items={namespaces}
               onSelect={handleNamespaceChange}
             />
           </div>
         </div>
-        
+
         <ForceGraph2D
           ref={fgRef}
           graphData={{ nodes: filteredNodes, links: [] }}
@@ -138,32 +138,37 @@ export const Topology = () => {
             const fontSize = 12 / globalScale;
             const radius = 10; // The radius for the nodes
             const metricValue = node[selectedMetric]; // Default to 0 if not specified
-          
+
             // Draw the outer circle (border)
             ctx.beginPath();
             ctx.arc(node.x, node.y, radius, 0, Math.PI * 2, false);
-            ctx.fillStyle = 'rgba(220, 220, 220, 0.9)'; // Light grey for the "empty" part
+            ctx.fillStyle = "rgba(220, 220, 220, 0.9)"; // Light grey for the "empty" part
             ctx.fill();
-          
+
             // Clipping area for the fill
             if (node[selectedMetric]) {
               ctx.save(); // Save the current context state
               ctx.beginPath();
               ctx.arc(node.x, node.y, radius, 0, Math.PI * 2, true);
               ctx.clip(); // Clip to the path
-          
+
               // Draw the filled part from the bottom
               ctx.beginPath();
-              ctx.rect(node.x - radius, node.y + radius, radius * 2, -radius * 2 * (metricValue / 100));
-              ctx.fillStyle = 'rgba(0, 123, 255, 0.9)'; // Blue color for the filled part
+              ctx.rect(
+                node.x - radius,
+                node.y + radius,
+                radius * 2,
+                -radius * 2 * (metricValue / 100)
+              );
+              ctx.fillStyle = "rgba(0, 123, 255, 0.9)"; // Blue color for the filled part
               ctx.fill();
-          
+
               ctx.restore(); // Restore the original state (removes clipping)
             }
-          
+
             // Draw the node label below the node
-            ctx.textAlign = 'center';
-            ctx.textBaseline = 'middle';
+            ctx.textAlign = "center";
+            ctx.textBaseline = "middle";
             ctx.fillStyle = "#000";
             ctx.font = `${fontSize}px Sans-Serif`;
             ctx.fillText(label, node.x, node.y + radius + 5); // Adjust label position to be below the node
@@ -172,5 +177,5 @@ export const Topology = () => {
         />
       </div>
     </div>
-);
-}
+  );
+};
