637dfc88f8fbdbe1842273b3452edf933ee24175
kpryu6 <kyongpyo0626@gmail.com>
2024-10-07 16:00:35 +0900

    [FEAT] add useQuery from react-query

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import './App.css';
-import Kanary from './Kanary';
+// App.js
+import "./App.css";
+import Kanary from "./Kanary";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+// Create a QueryClient instance
+const queryClient = new QueryClient();
 
 function App() {
   return (
+    <QueryClientProvider client={queryClient}>
       <div>
         <Kanary />
       </div>
+    </QueryClientProvider>
   );
 }
 
