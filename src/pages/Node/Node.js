332375988e45f23821d896c7f178be3d597e892f
kpryu6 <kyongpyo0626@gmail.com>
2024-11-08 16:15:46 +0900

    Delete Square3

--- src/pages/Node/Node.js
+++ src/pages/Node/Node.js
@@ -2,41 +2,12 @@ import React, { useState } from "react";
 import Sidebar from "../../Sidebar";
 import Navbar from "../../Navbar";
 import { CDBTable, CDBTableHeader, CDBTableBody } from "cdbreact";
-import { Line } from "react-chartjs-2";
 import "./Node.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
-import { chartOptions } from "../Chart";
 import { fetchData, confirm } from "../Utils";
 import { useQuery } from "react-query";
-import { Square1, Square2, Square3 } from "./Squares";
-
-// Sample data for the charts
-const cpuData = {
-  labels: ["00:00", "03:30", "07:00", "10:30", "14:00", "17:30", "21:00"],
-  datasets: [
-    {
-      label: "% CPU Usage",
-      data: [65, 59, 80, 81, 56, 55, 40],
-      fill: false,
-      backgroundColor: "rgba(75,192,192,0.4)",
-      borderColor: "rgba(75,192,192,1)",
-    },
-  ],
-};
-
-const ramData = {
-  labels: ["00:00", "03:30", "07:00", "10:30", "14:00", "17:30", "21:00"],
-  datasets: [
-    {
-      label: "% RAM Usage",
-      data: [45, 39, 60, 71, 46, 35, 30],
-      fill: false,
-      backgroundColor: "rgba(153,102,255,0.4)",
-      borderColor: "rgba(153,102,255,1)",
-    },
-  ],
-};
+import { Square1, Square2 } from "./Squares";
 
 export const Node = () => {
   const {
@@ -198,16 +169,6 @@ export const Node = () => {
                     {selectedRow.ram_capacity}GB
                   </Square2>
                   <Square2 topLeftText="Total Swap">{0}B</Square2>
-                  <Square3 topLeftText="% CPU Usage (Avg)">
-                    <div style={{ height: "200px" }}>
-                      <Line data={cpuData} options={chartOptions} />
-                    </div>
-                  </Square3>
-                  <Square3 topLeftText="% Memory Usage (Avg)">
-                    <div style={{ height: "200px" }}>
-                      <Line data={ramData} options={chartOptions} />
-                    </div>
-                  </Square3>
                 </div>
               </div>
             )}
